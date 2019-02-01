94b1dd458cf1313cd40e884782357f5575fbf918
Michał Siejak <michal@siejak.pl>
2019-02-01 23:22:02 +0100

    Implement QML module plugin to expose frontend types to QtQuick/QML

---
+++ src/qml/quick3draytrace/qtquick3draytraceplugin.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2018-2019 Michał Siejak
+ * This file is part of Quartz - a raytracing aspect for Qt3D.
+ * See LICENSE file for licensing information.
+ */
+
+#include <qtquick3draytraceplugin.h>
+#include <qqml.h>
+
+#include <Qt3DRaytrace/qgeometry.h>
+#include <Qt3DRaytrace/qgeometryrenderer.h>
+#include <Qt3DRaytrace/qmesh.h>
+
+void Qt3DQuick3DRaytracePlugin::registerTypes(const char *uri)
+{
+    // Geometry
+    qmlRegisterType<Qt3DRaytrace::QGeometry>(uri, 1, 0, "Geometry");
+    qmlRegisterType<Qt3DRaytrace::QGeometryRenderer>(uri, 1, 0, "GeometryRenderer");
+    qmlRegisterType<Qt3DRaytrace::QMesh>(uri, 1, 0, "Mesh");
+
+    // Module
+    qmlRegisterModule(uri, 1, 0);
+}
---
+++ src/qml/quick3draytrace/qtquick3draytraceplugin.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2018-2019 Michał Siejak
+ * This file is part of Quartz - a raytracing aspect for Qt3D.
+ * See LICENSE file for licensing information.
+ */
+
+#pragma once
+
+#include <QtQml/QQmlExtensionPlugin>
+
+class Qt3DQuick3DRaytracePlugin final : public QQmlExtensionPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
+public:
+    void registerTypes(const char *uri) override;
+};
