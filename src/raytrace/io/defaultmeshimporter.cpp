7f02cd7c2b26a612d427c056a11fb537fa6753f0
Michał Siejak <michal@siejak.pl>
2019-02-27 15:01:50 +0100

    IO: Improve robustness of skipping/re-normalizing invalid mesh data during import

--- src/raytrace/io/defaultmeshimporter.cpp
+++ src/raytrace/io/defaultmeshimporter.cpp
@@ -19,20 +19,19 @@
 #include <QMutexLocker>
 
 namespace Qt3DRaytrace {
-
-Q_LOGGING_CATEGORY(logImport, "raytrace.import")
-
 namespace Raytrace {
 
 static constexpr unsigned int ImportFlags =
+        aiProcess_GenNormals |
+        aiProcess_GenUVCoords |
+        aiProcess_TransformUVCoords |
         aiProcess_CalcTangentSpace |
         aiProcess_Triangulate |
         aiProcess_SortByPType |
+        aiProcess_JoinIdenticalVertices |
         aiProcess_PreTransformVertices |
-        aiProcess_GenNormals |
-        aiProcess_GenUVCoords |
-        aiProcess_OptimizeMeshes |
-        aiProcess_Debone |
+        aiProcess_FixInfacingNormals |
+        aiProcess_FindInvalidData |
         aiProcess_ValidateDataStructure;
 
 static constexpr QVector3D TangentGenUp{0.0f, 1.0f, 0.0f};
@@ -71,7 +70,7 @@ static bool importScene(const aiScene *scene, QGeometryData &data)
     meshBaseIndices.reserve(int(scene->mNumMeshes));
     for(int i=0; i<int(scene->mNumMeshes); ++i) {
         const aiMesh *mesh = scene->mMeshes[i];
-        if(mesh->HasPositions() && mesh->HasNormals()) {
+        if(mesh->HasPositions() && mesh->HasNormals() && mesh->mPrimitiveTypes == aiPrimitiveType_TRIANGLE) {
             meshBaseIndices.append(quint32(totalNumVertices));
             totalNumVertices += int(mesh->mNumVertices);
             totalNumFaces += int(mesh->mNumFaces);
@@ -84,23 +83,31 @@ static bool importScene(const aiScene *scene, QGeometryData &data)
     data.vertices.resize(totalNumVertices);
     data.faces.resize(totalNumFaces);
 
-    for(int i=0, vertexIndex=0, triangleIndex=0; i<int(scene->mNumMeshes); ++i) {
+    for(int i=0, meshIndex=0, vertexIndex=0, triangleIndex=0; i<int(scene->mNumMeshes); ++i) {
         const aiMesh *mesh = scene->mMeshes[i];
         if(!mesh->HasPositions() || !mesh->HasNormals()) {
             continue;
         }
+        if(mesh->mPrimitiveTypes != aiPrimitiveType_TRIANGLE) {
+            continue;
+        }
+
         for(int j=0; j<int(mesh->mNumVertices); ++j, ++vertexIndex) {
             auto &vertex = data.vertices[vertexIndex];
 
             vertex.position = { mesh->mVertices[j].x, mesh->mVertices[j].y, mesh->mVertices[j].z };
             vertex.normal = { mesh->mNormals[j].x, mesh->mNormals[j].y, mesh->mNormals[j].z };
+            vertex.normal.normalize();
             if(mesh->HasTextureCoords(0)) {
                 vertex.texcoord = { mesh->mTextureCoords[0][j].x, mesh->mTextureCoords[0][j].y };
             }
             if(mesh->HasTangentsAndBitangents()) {
                 vertex.tangent = { mesh->mTangents[j].x, mesh->mTangents[j].y, mesh->mTangents[j].z };
+                vertex.tangent.normalize();
             }
             else {
+                // Fallback when no UV coords were present so Assimp was unable to calculate tangents.
+                // Lousy tangents are better than no tangents. ;-)
                 vertex.tangent = QVector3D::crossProduct(TangentGenUp, vertex.normal);
                 if(vertex.tangent.lengthSquared() < TangentGenLengthThreshold) {
                     vertex.tangent = QVector3D::crossProduct(TangentGenRight, vertex.normal);
@@ -110,11 +117,13 @@ static bool importScene(const aiScene *scene, QGeometryData &data)
         }
         for(int j=0; j<int(mesh->mNumFaces); ++j, ++triangleIndex) {
             auto &triangle = data.faces[triangleIndex];
-            triangle.vertices[0] = meshBaseIndices[i] + mesh->mFaces[j].mIndices[0];
-            triangle.vertices[1] = meshBaseIndices[i] + mesh->mFaces[j].mIndices[1];
-            triangle.vertices[2] = meshBaseIndices[i] + mesh->mFaces[j].mIndices[2];
+            triangle.vertices[0] = meshBaseIndices[meshIndex] + mesh->mFaces[j].mIndices[0];
+            triangle.vertices[1] = meshBaseIndices[meshIndex] + mesh->mFaces[j].mIndices[1];
+            triangle.vertices[2] = meshBaseIndices[meshIndex] + mesh->mFaces[j].mIndices[2];
         }
+        ++meshIndex;
     }
+
     return true;
 }
 
@@ -122,16 +131,16 @@ bool DefaultMeshImporter::import(const QUrl &url, QGeometryData &data)
 {
     LogStream::initialize();
 
-    qCInfo(logImport) << "Loading mesh:" << url.toString();
-
     const aiScene *scene = nullptr;
     Assimp::Importer importer;
     {
         QFile sceneFile(url.path());
         if(!sceneFile.open(QFile::ReadOnly)) {
-            qCCritical(logImport) << "Cannot open mesh file:" << url;
+            qCCritical(logImport) << "Cannot open mesh file:" << url.toString();
             return false;
         }
+
+        qCInfo(logImport) << "Loading mesh:" << url.toString();
 
         const QByteArray sceneData = sceneFile.readAll();
         const QByteArray sceneHint = QFileInfo(url.path()).completeSuffix().toUtf8();
@@ -143,7 +152,7 @@ bool DefaultMeshImporter::import(const QUrl &url, QGeometryData &data)
         result = importScene(scene, data);
     }
     if(!result) {
-        qCCritical(logImport) << "Failed to import mesh from file:" << url;
+        qCCritical(logImport) << "Failed to import mesh from file:" << url.toString();
     }
     return result;
 }
