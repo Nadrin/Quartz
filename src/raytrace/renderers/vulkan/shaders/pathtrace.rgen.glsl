d5b21c72a445bd3c7f4c9c810022cf999775f1ac
Michał Siejak <michal@siejak.pl>
2019-03-04 13:31:29 +0100

    Vulkan: Move RNG out of generateCameraRay()

--- src/raytrace/renderers/vulkan/shaders/pathtrace.rgen.glsl
+++ src/raytrace/renderers/vulkan/shaders/pathtrace.rgen.glsl
@@ -27,7 +27,7 @@ vec3 cameraToWorld(vec3 v, vec3 right, vec3 up, vec3 forward)
     return right * v.x + up * v.y + forward * v.z;
 }
 
-void generateCameraRay(vec2 pixelLocation, inout RNG rng, out vec3 p, out vec3 wo)
+void generateCameraRay(vec2 pixelUV, vec2 lensUV, out vec3 p, out vec3 wo)
 {
     const vec3 up      = params.cameraUpVectorTanHalfFOV.xyz;
     const vec3 right   = params.cameraRightVectorLensR.xyz;
@@ -38,15 +38,15 @@ void generateCameraRay(vec2 pixelLocation, inout RNG rng, out vec3 p, out vec3 w
     const float lensRadius = params.cameraRightVectorLensR.w;
     const float lensFocalDistance = params.cameraForwardVectorLensF.w;
 
-    float tx = (2.0 * pixelLocation.x - 1.0) * tanHalfFOV * aspect;
-    float ty = (2.0 * pixelLocation.y - 1.0) * tanHalfFOV;
+    float tx = (2.0 * pixelUV.x - 1.0) * tanHalfFOV * aspect;
+    float ty = (2.0 * pixelUV.y - 1.0) * tanHalfFOV;
 
     vec3 pCamera  = vec3(0.0);
     vec3 woCamera = normalize(vec3(tx, ty, 1.0));
     if(lensRadius > 0.0) {
         float tFocus = lensFocalDistance / woCamera.z;
         vec3  pFocus = tFocus * woCamera;
-        pCamera.xy = lensRadius * sampleDisk(nextVec2(rng));
+        pCamera.xy = lensRadius * sampleDiskConcentric(lensUV);
         woCamera = normalize(pFocus - pCamera);
     }
     p  = cameraToWorld(pCamera, right, up, forward) + params.cameraPositionAspect.xyz;
@@ -64,10 +64,12 @@ void main()
     pPathTrace.depth = 0;
     pPathTrace.T     = vec3(1.0);
 
-    vec2 jitter = sampleRectangle(nextVec2(pPathTrace.rng), -0.5 * pixelSize, 0.5 * pixelSize);
+    vec2 jitter  = pixelSize * (nextVec2(pPathTrace.rng) - 0.5);
+    vec2 lensUV  = nextVec2(pPathTrace.rng);
+    vec2 pixelUV = pixelLocation + jitter;
 
     vec3 p, wo;
-    generateCameraRay(pixelLocation + jitter, pPathTrace.rng, p, wo);
+    generateCameraRay(pixelUV, lensUV, p, wo);
     traceNV(scene, gl_RayFlagsNoneNV, 0xFF, Shader_PathTraceHit, 1, Shader_PathTraceMiss, p, 0.0, wo, Infinity, 0);
 
     vec3 currentColor = prevColor + (pPathTrace.L - prevColor) / float(params.frameNumber);
