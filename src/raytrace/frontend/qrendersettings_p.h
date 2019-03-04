efd6cc09a3b6dd732d4118016ad02b78243bb762
Michał Siejak <michal@siejak.pl>
2019-03-04 13:35:47 +0100

    Frontend: Change default render settings minDepth/maxDepth to more performant values

--- src/raytrace/frontend/qrendersettings_p.h
+++ src/raytrace/frontend/qrendersettings_p.h
@@ -18,8 +18,8 @@ struct QRenderSettingsData
 
     int primarySamples = 1;
     int secondarySamples = 1;
-    int minDepth = 2;
-    int maxDepth = 4;
+    int minDepth = 1;
+    int maxDepth = 3;
 
     float directRadianceClamp = 0.0f;
     float indirectRadianceClamp = 0.0f;
